944ab55e6ba737fbdd108472b31ccf1c880a9a81
Ismail ZAHIR <ismailza407@gmail.com>
2024-03-26 17:56:50 +0000

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Listen on all network interfaces
+    port: 5173,
+    hmr: {
+      clientPort: 80, // This might be needed if you're facing HMR (Hot Module Replacement) issues
+    },
+  },
+});
