42ac46b7292b63c052d26517681a5638e3d2ad83
Ismail ZAHIR <ismailza407@gmail.com>
2024-03-26 18:09:37 +0000

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,21 @@ import './App.css'
 
 import Home from './pages/Home'
 import Login from './pages/Login'
+import Students from './pages/Students'
+import AuthLayout from './layout/AuthLayout'
+import GuestLayout from './layout/GuestLayout'
 
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/signin" element={<Login />} />
+      <Route element={<GuestLayout />}>
+        <Route path="/signin" element={<Login />} />
+      </Route>
+      <Route element={<AuthLayout />}>
+        <Route path="/students" element={<Students />} />
+      </Route>
     </Routes>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,15 +5,19 @@ import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { BrowserRouter } from 'react-router-dom'
 import AuthProvider from './context/AuthContext.jsx'
+import StudentProvider from './context/StudentContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
-        <ToastContainer />
+        <StudentProvider>
+          <App />
+          <ToastContainer />
+        </StudentProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
