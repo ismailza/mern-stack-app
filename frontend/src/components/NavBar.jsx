6c6e35f4f5ce29bfa53e126d00905cee5b04b607
Ismail ZAHIR <ismailza407@gmail.com>
2024-03-26 18:01:31 +0000

    update

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,7 +1,16 @@
 import { Link } from 'react-router-dom';
+import { useAuthContext } from '../context/AuthContext'; // Ensure the path matches your project structure
 import GitHubIcon from '@mui/icons-material/GitHub';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
-function NavBar() {
+const NavBar = () => {
+
+  const { user, logout } = useAuthContext();
+
+  const handleLogout = () => {
+    logout();
+  };
+
   return (
     <nav className='d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom'>
       <Link to='/' className='d-flex align-items-center text-decoration-none'>
@@ -10,11 +19,23 @@ function NavBar() {
 
       <nav className='d-inline-flex mt-2 mt-md-0 ms-md-auto'>
 
-        <Link to={'https://github.com/ismailza/mern-stack-app'} target='_blank' rel='noopener noreferrer' className='py-2 link-dark text-decoration-none'>
-          Ismail ZAHIR &nbsp; <GitHubIcon />
-        </Link>
+        {user ? (
+          <div className="dropdown">
+            <span className="dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+              <AccountCircleIcon /> &nbsp; {user.firstname} {user.lastname}
+            </span>
+            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <li><button className="dropdown-item" onClick={handleLogout}>Logout</button></li>
+            </ul>
+          </div>
+        ) : (
+          <Link to={'https://github.com/ismailza/mern-stack-app'} target='_blank' rel='noopener noreferrer' className='py-2 link-dark text-decoration-none'>
+            Ismail ZAHIR &nbsp; <GitHubIcon />
+          </Link>
+        )}
+
       </nav>
-    </nav>
+    </nav >
   );
 }
 
