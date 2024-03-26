d2bc73fc34c6445eb87d04657842b5e740373dc5
Ismail ZAHIR <ismailza407@gmail.com>
2024-03-26 12:00:51 +0000

    add student api routing

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,10 +1,17 @@
 import express from 'express';
 
 import { signin, signout } from '../controller/userController.js';
+import { findAllStudents, findStudentById, saveStudent, updateStudent, deleteStudent } from '../controller/studentController.js';
 
 const router = express.Router();
 
 router.post('/signin', signin);
 router.delete('/signout', signout);
+
+router.get('/students', findAllStudents);
+router.get('/student/:id', findStudentById);
+router.post('/student', saveStudent);
+router.put('/student/:id', updateStudent);
+router.delete('/student/:id', deleteStudent);
 
 export { router };
